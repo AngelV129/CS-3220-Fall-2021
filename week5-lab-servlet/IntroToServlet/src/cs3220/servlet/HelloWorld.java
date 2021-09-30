229a687ccafb77d2d7f4f4694a372db2960ce473
Angel Villalobos <angelvillalobos1995@gmail.com>
2021-09-29 17:09:14 -0700

    adding workplace

---
+++ week5-lab-servlet/IntroToServlet/src/cs3220/servlet/HelloWorld.java
@@ -0,0 +1,29 @@
+package cs3220.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({"/HelloWorld", "/member/*"})
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public HelloWorld() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Hello</title></head><body>");
+		out.println("<h2>Hello World</h2>");
+		out.println("</body></html>");
+	}
+
+}
---
+++ week5-lab-servlet/IntroToServlet/src/cs3220/servlet/RequestCounter.java
@@ -0,0 +1,33 @@
+package cs3220.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/RequestCounter")
+public class RequestCounter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	int counter = 0;
+	
+	public RequestCounter() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		++counter;
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>RequestCounter</title></head><body>");
+		out.println("<h2>This page has been viewed " + counter + " times</h2>");
+		out.println("</body></html>");
+		
+	}
+
+}
