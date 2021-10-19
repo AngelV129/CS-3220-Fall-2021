e05e7fdf5e23fd63bdf06f9aeb1a863b9b1edcb6
Angel Villalobos <angelvillalobos1995@gmail.com>
2021-10-18 21:25:17 -0700

    Added lecture src

---
+++ lecture-src/week-9/Jstl/src/cs3220/controller/PropertyTest.java
@@ -0,0 +1,38 @@
+package cs3220.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cs3220.model.A;
+import cs3220.model.B;
+
+@WebServlet("/PropertyTest")
+public class PropertyTest extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public PropertyTest()
+    {
+        super();
+    }
+
+    protected void doGet( HttpServletRequest request,
+        HttpServletResponse response ) throws ServletException, IOException
+    {
+        request.setAttribute( "a", new A() );
+        request.setAttribute( "b", new B() );
+        request.getRequestDispatcher( "/WEB-INF/PropertyTest.jsp" )
+            .forward( request, response );
+    }
+
+    protected void doPost( HttpServletRequest request,
+        HttpServletResponse response ) throws ServletException, IOException
+    {
+        doGet( request, response );
+    }
+
+}
